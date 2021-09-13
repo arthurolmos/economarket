07b3902ad9070fe6b6e683e2914046b4b839882d
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-09-13 11:17:10 -0300

    Fixing redis connection

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
 import { Notification } from './notification.entity';
 import { NotificationsResolver } from './notifications.resolver';
 import { NotificationsService } from './notifications.service';
 import { redisSubscription } from './subscription';
+import { redisOptions } from './subscription';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification]), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [redisOptions],
+    }),
+    TypeOrmModule.forFeature([Notification]),
+    UsersModule,
+  ],
   providers: [NotificationsService, NotificationsResolver, redisSubscription],
 })
 export class NotificationsModule {}
--- src/notifications/subscription.ts
+++ src/notifications/subscription.ts
@@ -1,25 +1,43 @@
 import { PubSub } from 'graphql-subscriptions';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import * as Redis from 'ioredis';
+import { ConfigService } from '@nestjs/config';
 
 // export const subscription = {
 //   provide: 'PUB_SUB',
 //   useValue: new PubSub(),
 // };
 
-const options = {
-  host: 'localhost',
-  port: 6379,
-  retryStrategy: (times) => {
-    // reconnect after
-    return Math.min(times * 50, 2000);
-  },
+interface RedisOptions {
+  options: {
+    host: string;
+    port: number;
+    retryStrategy: (times: number) => number;
+  };
+}
+
+export const redisOptions = (): RedisOptions => {
+  return {
+    options: {
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+      retryStrategy: (times) => {
+        // reconnect after
+        return Math.min(times * 50, 2000);
+      },
+    },
+  };
 };
 
 export const redisSubscription = {
   provide: 'REDIS_PUB_SUB',
-  useValue: new RedisPubSub({
-    publisher: new Redis(options),
-    subscriber: new Redis(options),
-  }),
+  useFactory: (configService: ConfigService) => {
+    const redisOptions = configService.get<RedisOptions>('options');
+
+    return new RedisPubSub({
+      publisher: new Redis(redisOptions.options),
+      subscriber: new Redis(redisOptions.options),
+    });
+  },
+  inject: [ConfigService],
 };
