835947f2bc20b078dff82448424781fb0726ba8f
Arthur Olmos Wosniaki <arthur.wosniaki@totvs.com.br>
2021-09-13 01:27:21 -0300

    Removed unused files

--- src/config/redis.ts
+++
@@ -1,9 +0,0 @@
-import { Transport, ClientProviderOptions } from '@nestjs/microservices';
-
-export const config: ClientProviderOptions = {
-  name: 'NOTIFICATION_SERVICE',
-  transport: Transport.REDIS,
-  options: {
-    url: 'redis://localhost:6379',
-  },
-};
